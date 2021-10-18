c8d6577d8ec98fdf36c1efbbd0d0585325c43f86
Haitham Assoli <74528836+haithamassoli@users.noreply.github.com>
2021-10-18 13:06:24 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,19 +5,18 @@ const emailErr = document.querySelector(".emailErr");
 const numberErr = document.querySelector(".numberErr");
 const passwordErr = document.querySelector(".passwordErr");
 
-console.log(emailErr);
-console.log(number);
-console.log(password);
+function removeError(err) {
+  err.innerHTML = "";
+}
 
 email.addEventListener("blur", (e) => {
-  console.log(e.target.value);
   try {
     if (e.target.value === "") throw "Email is empty";
-    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(e.target.value)) {
+    if (e.target.value !== "") removeError(emailErr);
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(e.target.value))
       throw "You have entered an invalid email address!";
-    } else {
-      throw "valid";
-    }
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(e.target.value))
+      removeError(emailErr);
   } catch (error) {
     emailErr.innerHTML = "Input is " + error;
   }
@@ -25,9 +24,10 @@ email.addEventListener("blur", (e) => {
 number.addEventListener("blur", (e) => {
   try {
     if (e.target.value === "") throw "Mobile is empty ";
-    if (e.target.value !== "") removeError();
+    if (e.target.value !== "") removeError(numberErr);
     if (!/077[0-9]{7}/.test(e.target.value))
       throw "You have entered an invalid number!";
+    if (/077[0-9]{7}/.test(e.target.value)) removeError(numberErr);
   } catch (error) {
     numberErr.innerHTML = "Input is " + error;
   }
@@ -35,8 +35,11 @@ number.addEventListener("blur", (e) => {
 password.addEventListener("blur", (e) => {
   try {
     if (e.target.value === "") throw "Password is empty";
+    if (e.target.value !== "") removeError(passwordErr);
     if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,18}$/.test(e.target.value))
       throw "You have entered an invalid password!";
+    if (/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,18}$/.test(e.target.value))
+      removeError(passwordErr);
   } catch (error) {
     passwordErr.innerHTML = "Input is " + error;
   }
